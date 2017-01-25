54bfaf24a89dcfad86e554df9d573c83d416e472
Ian Turton <ijturton@gmail.com>
2017-01-25 11:52:05 +0000

    Improvements for Issue #2
    https://github.com/AstunTechnology/mapserver-upgrade-config/issues/2
    Handle multiple validation lines.
    Also:
       added main method
       added automatic backup of inputfile
       added writing to output file

--- fix_mapfile.py
+++ fix_mapfile.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python
+
+import sys
+import shutil
+
 # open the MapFile
 class fix_mapfile:
     """A class to fix mapfiles while moving from 6.0 to 6.4.
@@ -7,17 +12,21 @@ class fix_mapfile:
     
      
     
-    :param filename: the name of the map file to fix
+    :param input: the name of the map file to fix
     :param width: number of spaces per indent level if using spaces (default 2)
     :param tabs: boolean should output be indented with tabs or spaces (default false)  
     
     """
-    def __init__(self,filename, width=2, tabs = False):
-        self.filename = filename
+    def __init__(self,input_file, output_file="", width=2, tabs = False):
+        self.input = input_file
+        self.output = output_file
         self.width = width
         self.tabs = tabs
+        if self.output:
+            sys.stdout = open(self.output,'w')
+            
         self.openings = ['class','validation','map','layer','outputformat', 'metadata','legend','label','querylayer','scalebar','web','style','querymap',
-                'projection']
+                'projection','symbol']
         
         
 
@@ -30,13 +39,13 @@ class fix_mapfile:
 
     
     def fix(self):
-        """prints the fixed file to std out
+        """prints the fixed file to std out or output file
     
         """
         in_meta = False
         indent = 0;
         pattern_line = ""
-        with open(self.filename) as fp:
+        with open(self.input) as fp:
             for line in fp:
                 sline = line.strip().lower()
                 if sline in self.openings:              
@@ -44,6 +53,7 @@ class fix_mapfile:
                     if('metadata' in sline):
                         in_meta = True
                         lines = []
+                        pattern_line = []
                     else:
                         print self.generate_indent(indent) + line.strip();
                     indent+=1
@@ -62,16 +72,17 @@ class fix_mapfile:
                         if pattern_line:
                             print self.generate_indent(indent)+"VALIDATION"
                             indent += 1
-                            print self.generate_indent(indent)+pattern_line
+                            for p in pattern_line:
+                                print self.generate_indent(indent)+p
                             indent -= 1
                             print self.generate_indent(indent)+"END"
                         line = '' # dispose of spare END
-                        pattern_line=''
+                        pattern_line=[]
                         
                 if 'validation_pattern' in sline:
                     bits=sline.split('_')
                     pattern = bits[-1].split()[-1]
-                    pattern_line = bits[0]+'" '+pattern
+                    pattern_line.append(bits[0]+'" '+pattern)
                     line='' # delete the line from the input
                         
                 if (line.rstrip() != '' and not in_meta):
@@ -80,3 +91,14 @@ class fix_mapfile:
                 if(line.rstrip() != '' and in_meta):
                     lines.append(line.strip())
 
+
+def main():
+    for arg in sys.argv[1:]:
+        #copy to backup and run fix
+        shutil.copy(arg, arg+".bak")
+        fixer = fix_mapfile(arg+".bak",arg)
+        fixer.fix()
+     
+
+if __name__ == "__main__":
+    main()        
