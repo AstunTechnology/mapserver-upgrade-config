dab40c018a9324bc7e44c7cae1646c358077cb59
Ian Turton <ijturton@gmail.com>
2017-10-18 16:15:45 +0100

    added eggs

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='maputils',
+      version='0.2.1',
+      description='Useful scripts for mapfile manipulation',
+      url='',
+      author='Ian Turton',
+      author_email='ianturton@astuntechnology.com',
+      license='MIT',
+      packages=['maputils'],
+      zip_safe=False)
