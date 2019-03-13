37c6c0bbe3a06c45b26f63545e8dc671b92cc50d
Ian Turton <ijturton@gmail.com>
2019-03-13 15:14:27 +0000

    fix unicode issue and clean up code to PEP8

--- maputils/map_loader.py
+++ maputils/map_loader.py
@@ -7,19 +7,21 @@ import sys
 import argparse
 import mappyfile
 import json
+import io
+
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument( "mapfile" )
+    parser.add_argument("mapfile")
     args = parser.parse_args()
-    mapfile = open( args.mapfile, "r" )
-    mf = mappyfile.load( mapfile)
+    mapfile = io.open(args.mapfile, "r", encoding="utf-8")
+    mf = mappyfile.load(mapfile)
 
-    if len(sys.argv)==3:
+    if len(sys.argv) == 3:
         sys.stdout = open(sys.argv[2], 'w')
 
     json.dump(mf, sys.stdout, indent=4)
 
-if __name__ == "__main__":
-    main()        
 
+if __name__ == "__main__":
+    main()
