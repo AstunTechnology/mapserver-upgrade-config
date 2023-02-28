68007569213e54d93360bd0e7047907acb988cb4
Ian Turton <ijturton@gmail.com>
2023-02-28 14:13:09 +0000

    missing setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mapfile-utils',
-      version='2.4.4',
+      version='2.4.5',
       description='Useful scripts for mapfile manipulation',
       url='',
       author='Ian Turton',
